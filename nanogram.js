be65eb789b66c8f236367a1686bb49ba649f8d28
juliamanska <julia.ma@onet.com.pl>
2023-12-08 12:04:36 +0100

    Implement logic for toggling cell appearance on click and right-click

--- nanogram.js
+++ nanogram.js
@@ -5,7 +5,34 @@ for (let i = 1; i <= 10; i++) {
 
   for (let j = 1; j <= 11; j++) {
     const cell = document.createElement("td");
+
+    if (j <= 11 && j > 1) {
+      cell.classList.add("cell");
+    }
     row.appendChild(cell);
   }
   tableBody.appendChild(row);
 }
+
+const cellsToColor = document.querySelectorAll(".cell");
+
+cellsToColor.forEach((cell) => {
+  cell.addEventListener("click", (e) => {
+    if (e.target.style.backgroundColor === "black") {
+      e.target.style.backgroundColor = "#fce9ec";
+    } else if (e.target.innerHTML === "X") {
+      e.target.innerHTML = "";
+      e.target.style.backgroundColor = "black";
+    } else {
+      e.target.style.backgroundColor = "black";
+    }
+  });
+});
+
+cellsToColor.forEach((cell) => {
+  cell.addEventListener("contextmenu", (e) => {
+    e.preventDefault();
+    e.target.style.backgroundColor = "#fce9ec";
+    e.target.innerHTML = "X";
+  });
+});
