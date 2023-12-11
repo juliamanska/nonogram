719ffbf16a7312be244ea7f94cf79ca041749552
juliamanska <julia.ma@onet.com.pl>
2023-12-11 14:52:57 +0100

    Define id of winning cells

--- nonogram.js
+++ nonogram.js
@@ -11,6 +11,11 @@ const numbersInRows = [
   "1 1",
   "1 1",
 ];
+const winCells = [
+  18, 26, 27, 28, 29, 35, 36, 38, 44, 45, 48, 53, 54, 55, 57, 58, 510, 62, 63,
+  64, 65, 66, 67, 68, 69, 71, 72, 73, 74, 75, 76, 77, 78, 710, 81, 82, 84, 85,
+  86, 87, 92, 93, 94, 95, 103,
+];
 
 for (let r = 1; r <= 10; r++) {
   const row = document.createElement("tr");
@@ -20,6 +25,9 @@ for (let r = 1; r <= 10; r++) {
 
   for (let c = 1; c <= 11; c++) {
     const cell = document.createElement("td");
+    const cellId = `${c - 1}${r}`;
+
+    cell.id = cellId;
     if (c === 1) {
       cell.classList.add("numbers");
       cell.textContent = numbersInRows[r - 1];
